67b6eac619020e500d6d2733a6e67e9c0a17d958
elgoatinho <118262905+elgoatinho@users.noreply.github.com>
2022-11-30 15:54:21 +0100

    gitignor c make

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/nahue/CLionProjects/untitled22
+# Install script for directory: C:/Users/nahue/OneDrive/Documents/GitHub/BatailleNaval
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/nahue/CLionProjects/untitled22/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/nahue/OneDrive/Documents/GitHub/BatailleNaval/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
