157148164b2f7fa932ff9f53ad86a8ae047b45d4
Cohru <117997168+Cohru@users.noreply.github.com>
2022-12-02 19:52:51 +0100

    sauvegarde

---
+++ Sauvegardeheader.h
@@ -0,0 +1,9 @@
+#include "grilleheader.h"
+#include "bateauheader.h"
+#include "Partieheader.h"
+
+
+#ifndef UPDATED_UPSTREAM_SAUVEGARDEHEADER_H
+#define UPDATED_UPSTREAM_SAUVEGARDEHEADER_H
+
+#endif //UPDATED_UPSTREAM_SAUVEGARDEHEADER_H
