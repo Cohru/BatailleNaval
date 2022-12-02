7c1feea84ecc8ffb0497a923659678aac42ac99a
elgoatinho <118262905+elgoatinho@users.noreply.github.com>
2022-12-02 23:03:58 +0100

    aaaa

--- menu.c
+++ menu.c
@@ -3,6 +3,23 @@
 //
 
 #include "menu.h"
+void clear() {
+    COORD topLeft  = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(
+            console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    FillConsoleOutputAttribute(
+            console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+            screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    SetConsoleCursorPosition(console, topLeft);
+}
+
 void gotoligcol( int lig, int col )
 {
 // ressources
@@ -27,6 +44,7 @@ int Menu()
     gotoligcol(1,35);
     printf("-------------------BATAILLE NAVALE---------------------\n");
 
+
     do
     {
         Color(1, 0);
@@ -42,11 +60,16 @@ int Menu()
         Color(12, 0);
         printf("Appuyer sur le numero correspondant !\n");
 
-        fflush(stdin);
 
+
+        fflush(stdin);
         scanf("%d", &ChoixMode);
+
     }
+
+
     while(ChoixMode<1 || ChoixMode>4);
+
 
     if (ChoixMode == 1)
     {
@@ -55,17 +78,19 @@ int Menu()
     }
     else if (ChoixMode == 2)
     {
+
         printf("Vous reprenez votre derniere partie\n");
     }
     else if (ChoixMode == 3)
     {
-        printf("Voici les regles du jeu !\n");
+        clear();
         aide();
 
 
     }
     else if (ChoixMode == 4)
     {
+
         printf("A bientot!\n");
         exit (0);
     }
