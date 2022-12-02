5b7d0edf708034b685b311571cac8ac6f18f6f82
Cohru <117997168+Cohru@users.noreply.github.com>
2022-12-02 22:22:09 +0100

    test ecriture de la sauvegarde binaire

--- Sauvegardeheader.h
+++ Sauvegardeheader.h
@@ -2,6 +2,14 @@
 #include "bateauheader.h"
 #include "Partieheader.h"
 
+typedef struct{
+    char joueur[16][16];
+    char affichage[16][16];
+    char adversaire[16][16];
+    t_bateau *bateau;
+    t_bateau *bateauad;
+    int n_fusee;
+}t_save;
 
 #ifndef UPDATED_UPSTREAM_SAUVEGARDEHEADER_H
 #define UPDATED_UPSTREAM_SAUVEGARDEHEADER_H
--- sauvegarde.c
+++ sauvegarde.c
@@ -1,1 +1,29 @@
 #include "Sauvegardeheader.h"
+
+void Ecriredatasave(t_save *partie){
+    char n_partie[]={"save/save.dat"};
+    t_save partie_data;
+    partie_data= *partie;
+    FILE * fp=fopen(n_partie,"wb");
+    if(!fp){
+        printf("errreur d'ouverture du fichier\n");
+    }
+    fwrite(&partie_data,sizeof(t_save),1,fp);
+    fclose(fp);
+}
+
+t_save Liredatasave(char *n_partie){
+    t_save partie_data;
+    FILE * fp;
+    fp= fopen(n_partie,"rb");
+    if(!fp){
+        printf("erreur d'ouverture du fichier\n");
+    }
+    while(1){
+        fread(&partie_data,sizeof(t_save),1,fp);
+        if(feof(fp)){
+            break;
+        }
+    }
+    return partie_data;
+}
